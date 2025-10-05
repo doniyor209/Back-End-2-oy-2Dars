5ee1fa135c1ce1fff9f767883bb5b370b99f9366
doniyor209 <doniyortoshev123@gmail.com>
2025-10-05 11:19:35 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,71 @@
+# name="Doniyor"
+
+# age=2025
+# age1=16
+# print(f"Mening ismim {name} va men {age-age1} to'gilganman")
+
+
+
+
+# name="Doniyor"
+# age=24
+
+# print(f"Mening ismim{name} va meni yoshim{age}")
+
+
+
+
+
+# a = 5
+# b = 4
+
+# c = f"{b} {a}"
+# print(c)
+
+
+
+
+# a = 5
+# b = 4
+
+# lst = [b, a]
+# print(*lst)
+
+
+
+
+
+
+
+
+
+
+
+
+                                  # HOMEWORK
+
+
+
+
+
+print("Ikki sonning yig‘indisi, ayirmasi, ko‘paytmasi va bo‘linmasi:")
+print("a = 10, b = 5")
+print("Yig'indisi:", 10 + 5)
+print("Ayirmasi:", 10 - 5)
+print("Ko‘paytmasi:", 10 * 5)
+print("Bo‘linmasi:", 10 / 5)
+
+print("Ism: Doniyor")
+print("Yosh: 18")
+
+print("a = 17, b = 5")
+print("Butun qismi:", 17 // 5)
+print("Qoldiq qismi:", 17 % 5)
+
+print("r = 7")
+print("Yuza:", 3.14 * 7 * 7)
+print("Uzunlik:", 2 * 3.14 * 7)
+
+print("Son = 4")
+print("Kvadrati:", 4 ** 2)
+print("Kubi:", 4 ** 3)
